36ba454dcc58900765f3b2afe52f25ace93f89f6
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2025-09-29 00:58:27 +0500

    add - conditions

--- src/options/create.ts
+++ src/options/create.ts
@@ -2,60 +2,77 @@ import inquirer from "inquirer"
 import chalk from "chalk"
 import db from "../db/index.ts"
 import { restartProgram } from "../index.ts"
+import boxen from "boxen"
 
 const createStreak = async () => {
     console.clear()
-    console.log(chalk.magentaBright.bold('Create your Streak'))
-    const { name, days, purpose } = await inquirer.prompt([
-        {
-            name: "name",
-            type: "input",
-            message: chalk.green("Enter the name of your streak:"),
-            required: true
-        },
-        {
-            name: "purpose",
-            type: "input",
-            message: chalk.green("Define the purpose of your streak (be specific):"),
-            required: true
-        },
-        {
-            name: "days",
-            type: "number",
-            message: chalk.green("Enter the number of days your streak will last:"),
-            required: true,
-            min: 3,
-            max: 400
-        }
-    ])
+    await db.read();
 
-
-
-
-
-    if (name && days && purpose) {
-        const today = new Date().toISOString().split("T")[0];
-        await db.read();
-
-        if (!db.data) {
-            db.data = { xp: 0, level: 1, streaks: [] };
-        }
-
-        db.data.streaks.push({
-            id: Date.now(),
-            name: name,
-            purpose: purpose,
-            createdAt: today,
-            daysLogs: [],
-            xpPerStreak: 0,
-            numberOfDays: days
-        })
-
-        await db.write()
-        console.log(chalk.green("✅ Your Streak has been created you can check your streak by clicking View Streaks >> Your Streak Name"))
+    if (db.data.streaks.length >= 3) {
+        console.log(chalk.red.bold("You can only have 3 streaks at a time!"))
         restartProgram()
     } else {
-        console.log(chalk.red("Please provide the required fields"))
+        console.log(chalk.magentaBright.bold('Create your Streak'))
+        const { name, days, purpose } = await inquirer.prompt([
+            {
+                name: "name",
+                type: "input",
+                message: chalk.green("Enter the name of your streak:"),
+                required: true
+            },
+            {
+                name: "purpose",
+                type: "input",
+                message: chalk.green("Define the purpose of your streak (be specific):"),
+                required: true
+            },
+            {
+                name: "days",
+                type: "number",
+                message: chalk.green("Enter the number of days your streak will last:"),
+                required: true,
+                min: 3,
+                max: 400
+            }
+        ])
+
+
+        if (name && days && purpose) {
+            const today = new Date().toISOString().split("T")[0];
+
+
+            if (!db.data) {
+                db.data = { xp: 0, level: 1, streaks: [] };
+            }
+
+            db.data.streaks.push({
+                id: Date.now(),
+                name: name,
+                purpose: purpose,
+                createdAt: today,
+                daysLogs: [],
+                xpPerStreak: 0,
+                numberOfDays: days
+            })
+
+            await db.write()
+            const message = chalk.cyanBright(`
+                ✅ Your Streak "Workout" has been created successfully!
+                🔥 Remember: consistency beats motivation —
+                every day you log this streak, you're leveling up your discipline!
+                Keep pushing, you've got this! 💪
+                `);
+            console.log(boxen(message, {
+                padding: 1,
+                margin: 1,
+                borderStyle: "double",
+                borderColor: "magenta",
+                backgroundColor: "black"
+            }));
+            restartProgram()
+        } else {
+            console.log(chalk.red("Please provide the required fields"))
+        }
     }
 }
 
--- src/options/list.ts
+++ src/options/list.ts
@@ -1,22 +1,32 @@
 import inquirer from "inquirer";
 import chalk from "chalk"
 import db from "../db/index.ts";
+import { mainMenu, restartProgram } from "../index.ts";
 
 const list = async () => {
-    console.clear()
-    console.log(chalk.magentaBright.bold('Your Streaks'))
     await db.read()
 
-    const { streak } = await inquirer.prompt([{
-        name: "streak",
-        type: "list",
-        message: chalk.green.bold("Select a list for further options:"),
-        choices: [...db.data.streaks.map((s) => ({ name: s.name, value: s.id })), {name: "Go To Main Menu", value: "main"}]
-    }])
+    if (db.data.streaks.length === 0) {
+        console.log(chalk.red.bold("You don't have any streaks"))
+        restartProgram()
+    } else {
+        console.clear()
+        console.log(chalk.magentaBright.bold('Your Streaks'))
+        const { streak } = await inquirer.prompt([{
+            name: "streak",
+            type: "list",
+            message: chalk.green.bold("Which streaks you wanna preview?"),
+            choices: [...db.data.streaks.map((s) => ({ name: s.name, value: s.id })), { name: "Go To Main Menu", value: "main" }]
+        }])
 
 
-    console.log(streak)
+        if (typeof streak === "number") {
+            const streakData = db.data.streaks.find(s => s.id === streak)
 
+        } else {
+            mainMenu()
+        }
+    }
 }
 
 export default list
